06671d6d3540ca76135bc96b0751d465ce031b18
Narendra523116 <nalamothunarendra@gmail.com>
2025-08-12 20:44:03 +0530

    correcting agent.tsx for wapi

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -88,16 +88,15 @@ const Agent = ({
         if (type === "generate") {
             await vapi.start(
                 undefined,
+                undefined,
+                undefined,
+                process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,
                 {
                     variableValues: {
                         username: userName,
                         userid: userId,
                     },
-                    clientMessages: ["transcript"],
-                    serverMessages: [],
-                },
-                undefined,
-                generator
+                }
             );
         } else {
             let formattedQuestions = "";
@@ -111,8 +110,6 @@ const Agent = ({
                 variableValues: {
                     questions: formattedQuestions,
                 },
-                clientMessages: ["transcript"],
-                serverMessages: [],
             });
         }
     };
